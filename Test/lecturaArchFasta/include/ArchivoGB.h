3fc1336b5a4200ed7e2a4a86542982c5cfdff2c3
Karla <kavilac.7@hotmail.com>
2015-10-25 10:24:17 -0600

    programa para lectura de archivos fasta

---
+++ Test/lecturaArchFasta/include/ArchivoGB.h
@@ -0,0 +1,15 @@
+#ifndef ARCHIVOGB_H
+#define ARCHIVOGB_H
+#include <string>
+using namespace std;
+
+class ArchivoGB
+{
+    public:
+        ArchivoGB();
+       // void leerArchivo(string nombreArchivo);
+    private:
+
+};
+
+#endif // ARCHIVOGB_H
